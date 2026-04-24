420fbe8e734940fc042e44150fa60acaf33e4d32
tranvanson231099 <tranvanson231099@gmail.com>
2026-04-24 13:57:19 +0000

    a

--- background.js
+++ background.js
@@ -1,34 +1,15 @@
-let wasmExports;
+const engineID = "unikey_ime";
 
-async function loadWasm() {
-  try {
-    const wasmModule = await fetch(chrome.runtime.getURL('engine.wasm'));
-    const wasmInstance = await WebAssembly.instantiate(await wasmModule.arrayBuffer());
-    wasmExports = wasmInstance.exports;
-  } catch (error) {
-    console.error("Failed to load Wasm module:", error);
-  }
-}
+chrome.input.ime.onFocus.addListener((context) => {
+  // TODO: Add logic to handle when a text field is focused
+});
 
-loadWasm();
+chrome.input.ime.onBlur.addListener((contextID) => {
+  // TODO: Add logic to handle when a text field loses focus
+});
 
-function processKeyWithWasm(key) {
-  if (wasmExports && wasmExports.process_key) {
-    // This is a placeholder for how you might interact with your Wasm module.
-    // You'll need to adapt this based on the actual functions in your engine.wasm.
-    // For example, you might need to manage memory for string passing.
-    console.log(`Sending key '${key}' to Wasm.`);
-    // const result = wasmExports.process_key(key.charCodeAt(0)); 
-    // console.log(`Wasm returned: ${result}`);
-  }
-}
-
-chrome.input.ime.onKeyEvent.addListener(
-  (engineID, keyData) => {
-    console.log('Key event:', keyData);
-    if (keyData.type === 'keydown') {
-      processKeyWithWasm(keyData.key);
-    }
-    return false;
-  }
-);
+chrome.input.ime.onKeyEvent.addListener((engineID, keyData) => {
+  // TODO: This is where we will process keystrokes
+  // for now, just return false to indicate we haven't handled the event
+  return false;
+});
